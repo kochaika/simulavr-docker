747fecffe18d4665708e2cb4a92c36f27a3f38c4
Konstantin Chaika <pro100kot14@gmail.com>
2024-03-08 12:53:12 +0400

    Add timer example

--- avr-project/simple.c
+++ avr-project/simple.c
@@ -1,3 +1,5 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
 // Based on: https://www.nongnu.org/simulavr/simple_ex.html
 /* This port corresponds to the "-W 0x20,-" command line option. */
 #define special_output_port (*((volatile char *)0x20))
@@ -13,8 +15,33 @@ void debug_puts(const char *str) {
         special_output_port = *c;
 }
 
+void start_system_timer(void)
+{
+    TCNT0 = 0;   // Value of counter
+    OCR0A = 250;  // Divisor. 1ms on 4MHz, CTC mode
+    TCCR0B = (1 << CS01) | (1 << CS00);// clkT0S/64 (From prescaler)
+    TCCR0A = (1 << WGM01);
+    TIMSK0 = (1 << OCIE0A);  // Timer/Counter0 Output Compare Match Interrupt Enable
+
+    // button
+    PCICR |= (1 << PCIE2);     // set PCIE0 to enable PCMSK0 scan
+    PCMSK2 |= (1 << PCINT2);   // set PCINT0 to trigger an interrupt on state change
+
+    sei();
+}
+
+ISR(TIMER0_COMPA_vect){
+    cli();
+    debug_puts("Interrupt!\n");
+    sei();
+}
+
 /* Main for test program.  Enter a string and echo it. */
 int main() {
+//    start_system_timer();
+//    while(1){
+//
+//    }
     volatile char in_char;
 
     /* Output the prompt string */
